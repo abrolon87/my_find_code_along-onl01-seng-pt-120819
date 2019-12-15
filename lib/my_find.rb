3e83d7288e96f3e2f9ce5c7158bbfa3d21518eca
Amanda Rolon <abrolon87@gmail.com>
2019-12-15 04:37:52 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -10,16 +10,4 @@ def my_find(collection)
   end
 end
 
-# def my_all?(collection)
-#   i = 0
-#   block_return_values = []
-#   while i < collection.length
-#     block_return_values << yield(collection[i])
-#     i += 1
-#   end
-#   if block_return_values.include?(false)
-#     false
-#   else
-#     true
-#   end
-# end
+
